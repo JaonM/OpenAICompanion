669c6a8259add3f5eb2f8d97f1b6a9dca228dac1
马强 <maqiangcsu@gmail.com>
2026-08-26 16:56:11 +0800

    refactor(kmp): remove unused placeholder layer

--- kmp/src/desktopMain/kotlin/com/harness/kmp/Platform.kt
+++
@@ -1,8 +0,0 @@
-package com.harness.kmp
-
-private class DesktopPlatform : Platform {
-    override val name: String = System.getProperty("os.name") ?: "Desktop"
-    override val version: String = System.getProperty("os.version") ?: "unknown"
-}
-
-actual fun getPlatform(): Platform = DesktopPlatform()
--- kmp/src/wasmJsMain/kotlin/com/harness/kmp/Platform.kt
+++
@@ -1,8 +0,0 @@
-package com.harness.kmp
-
-private class WasmJsPlatform : Platform {
-    override val name: String = "Wasm/JS"
-    override val version: String = "1.0"
-}
-
-actual fun getPlatform(): Platform = WasmJsPlatform()
